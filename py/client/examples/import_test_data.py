ee8944cde2b27caf67493c478a88d697b7180ea6
SuperTails <sciencedude2003@gmail.com>
2022-06-01 09:33:40 -0600

    Fixed examples

--- py/client/examples/import_test_data.py
+++ py/client/examples/import_test_data.py
@@ -9,9 +9,9 @@
 
 """
 
-from pyarrow import csv
+from pyarrow import parquet
 
-from examples.csv_downloader import download_csv
+from examples.file_downloader import download_file
 from pydeephaven import Session, Table
 from pydeephaven.utils import is_deephaven_compatible
 
@@ -19,13 +19,17 @@ from pydeephaven.utils import is_deephaven_compatible
 def import_taxi_records(dh_session: Session) -> Table:
 
     # download the CSV data and read it into a pyarrow table and prepare it for uploading into DH
-    csv_file_name = download_csv(url="https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2020-12.csv")
-    pa_table = csv.read_csv(csv_file_name)
+    csv_file_name = download_file(url="https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2020-12.parquet", file_name="sample.parquet")
+    pa_table = parquet.read_table(csv_file_name)
 
     # drop unwanted columns
     unwanted_columns = ["tpep_pickup_datetime", "tpep_dropoff_datetime", "RatecodeID",
                         "store_and_fwd_flag", "PULocationID", "DOLocationID"]
     pa_table = pa_table.drop(unwanted_columns)
+
+    # this is necessary because otherwise the data is too big to be sent
+    new_len = pa_table.num_rows // 2
+    pa_table = pa_table.slice(length=new_len)
 
     # drop any column with a unsupported data type
     for column, column_name in zip(pa_table.columns, pa_table.column_names):
